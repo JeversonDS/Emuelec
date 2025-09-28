4692f90b6805bbc071a9b6f346a10967317f0e1d
retroplay.linux <retroplay.on@gmail.com>
2025-09-28 14:40:32 -0300

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Curirtiba 28 de Setembro de 2025.
+# Editor: Jeverson D. da Silva   ///@JCGAMESCLASSICOS...
+
+
+url=https://github.com/JeversonDS/Emuelec/releases/download/v1.0/python-pygame.zip
+squash_pygame=(basename "$url")
+wget "$url"
+unzip "$squash_pygame"
+rm -f "$squash_pygame"
+
+chmod -R 777 EMUELEC
+cd EMUELEC
+./instalador.sh
+./coin.sh
